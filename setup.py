9d33d5efbeeebc874a6d48f7f4e8e2b4a023f264
Ptrskay3 <leeh123peter@gmail.com>
2019-08-28 18:32:06 +0200

    added files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Interferometry-PtrSkay",
+    version="0.0.1",
+    author="Péter Leéh",
+    author_email="leeh123peter@gmail.com",
+    description="UI for specrally refined interferometry",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ptrskay3/Interferometry",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+
+
+)
