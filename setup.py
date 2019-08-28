e715f239c0d1d4944690094be3543888f241441a
Ptrskay3 <leeh123peter@gmail.com>
2019-08-28 19:06:40 +0200

    .

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Ptrskay3/Interferometry",
+    install_requires=[
+          'numpy','scipy', 'matplotlib', 'pandas', 'PyQt5', 'lmfit'
+      ],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
