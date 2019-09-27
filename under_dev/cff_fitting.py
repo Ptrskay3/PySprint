54723be1c3ad334d4c81934136bd4fdf5544e541
Ptrskay3 <leeh123peter@gmail.com>
2019-09-27 20:36:52 +0200

    Added CI

--- under_dev/cff_fitting.py
+++ under_dev/cff_fitting.py
@@ -8,13 +8,9 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt 
 
 def find_nearest(array, value):
-	#Finds the nearest element to the given value in the array
-	#returns tuple: (element, element's index)
-	
     array = np.asarray(array)
     idx = (np.abs(value - array)).argmin()
     return array[idx], idx
-
 
 def cos_fit1(x,c0, c1, b0, b1):
 	return c0 + c1*np.cos(b0 + b1*x)
@@ -32,8 +28,8 @@ def cos_fit3(x,c0, c1, b0, b1, b2, b3):
 	return c0 + c1*np.cos(b0 + b1*x + b2*x**2 + b3*x**3)
 
 class FitOptimizer(object):
-
-	def __init__(self, x, y, ref, sam, func = None, p0 = None, outfunc= None):
+	"""Class to help achieve better fitting results."""
+	def __init__(self, x, y, ref, sam, func=None, p0=None, outfunc=None):
 		self.x = x
 		self.y = y
 		self.ref = ref
@@ -70,40 +66,36 @@ class FitOptimizer(object):
 		self.popt = self.p0
 		self._init_set = False
 		self.counter = 0
-		# self.obj = None
-		# self.outfunc = None
-
 
 	def set_initial_region(self, percent, center):
 		""" Determines the initial region to fit"""
 		self._init_set = True
 		_, idx = find_nearest(self.x, center)
-		self.upper_bound = np.floor(idx + (percent/2)*(len(self.x) + 1))
-		self.lower_bound = np.floor(idx - (percent/2)*(len(self.x) + 1))
-		self.upper_bound = self.upper_bound.astype(int)
-		self.lower_bound = self.lower_bound.astype(int)
-		if self.lower_bound < 0:
-			self.lower_bound = 0
-		if self.upper_bound > len(self.x):
-			self.upper_bound = len(self.x)
-		self._x_curr = self.x[self.lower_bound:self.upper_bound]
-		self._y_curr = self._y_norm[self.lower_bound:self.upper_bound]
+		self._upper_bound = np.floor(idx + (percent/2)*(len(self.x) + 1))
+		self._lower_bound = np.floor(idx - (percent/2)*(len(self.x) + 1))
+		self._upper_bound = self._upper_bound.astype(int)
+		self._lower_bound = self._lower_bound.astype(int)
+		if self._lower_bound < 0:
+			self._lower_bound = 0
+		if self._upper_bound > len(self.x):
+			self._upper_bound = len(self.x)
+		self._x_curr = self.x[self._lower_bound:self._upper_bound]
+		self._y_curr = self._y_norm[self._lower_bound:self._upper_bound]
 		
-
-	def _extend_region(self, extend_by = 0.2):
+	def _extend_region(self, extend_by=0.2):
 		""" Extends region of fit"""
-		self.new_lower = np.floor(self.lower_bound - extend_by*len(self.x))
-		self.new_upper = np.floor(self.upper_bound + extend_by*len(self.x))
-		self.new_lower = self.new_lower.astype(int)
-		self.new_upper = self.new_upper.astype(int)
-		self.lower_bound = self.new_lower
-		self.upper_bound = self.new_upper
-		if self.new_lower < 0:
-			self.new_lower = 0
-		if self.new_upper > len(self.x):
-			self.new_upper = len(self.x)
-		self._x_curr = self.x[self.new_lower:self.new_upper]
-		self._y_curr = self._y_norm[self.new_lower:self.new_upper]
+		self._new_lower = np.floor(self._lower_bound - extend_by*len(self.x))
+		self._new_upper = np.floor(self._upper_bound + extend_by*len(self.x))
+		self._new_lower = self._new_lower.astype(int)
+		self._new_upper = self._new_upper.astype(int)
+		self._lower_bound = self._new_lower
+		self._upper_bound = self._new_upper
+		if self._new_lower < 0:
+			self._new_lower = 0
+		if self._new_upper > len(self.x):
+			self._new_upper = len(self.x)
+		self._x_curr = self.x[self._new_lower:self._new_upper]
+		self._y_curr = self._y_norm[self._new_lower:self._new_upper]
 
 	def _make_fit(self):
 		""" Makes fit  """
@@ -113,28 +105,17 @@ class FitOptimizer(object):
 			self.popt, self.pcov = curve_fit(self.func, self._x_curr, self._y_curr, maxfev = 200000, p0 = self.p0)
 			self.p0 = self.popt 
 		except RuntimeError:
-			if len(self.popt)> 4:
+			if len(self.popt) > 4:
 				self.p0[:3] = self.popt[:3] + np.random.normal(0, 100, len(self.popt)-3)
 			else:
 				self.p0 = self.popt + np.random.normal(0,100, len(self.popt))
 			self.popt, self.pcov = curve_fit(self.func, self._x_curr, self._y_curr, maxfev = 200000, p0 = self.p0)
-
-
-	# def _perturb_param(self, which = 2):
-		# self.p0 = self.popt
-		# if which == 0:
-			# pass
-		# else:
-			# self.p0[which] = np.random.normal(0, 10**(which+1), 1)
-
-
 
 	def _fit_goodness(self):
 		residuals = self._y_curr - self.func(self._x_curr, *self.popt)
 		ss_res = np.sum(residuals**2)
 		ss_tot = np.sum((self._y_curr - np.mean(self._y_curr))**2)
 		return 1 - (ss_res / ss_tot)
-
 	
 	def show_fit(self, obj = None):
 		try:
@@ -144,12 +125,10 @@ class FitOptimizer(object):
 		except:
 			pass
 
-
-	def run_loop(self, r_extend_by, r_threshold, outfunc, max_tries = 1000, show_steps = False):
+	def run_loop(self, r_extend_by, r_threshold, outfunc, max_tries = 10000, show_steps = False):
 		if self._init_set == False:
 			raise ValueError('Set the initial conditions.')
 		self._make_fit()
-		self.show_fit(self.obj)
 		while self._fit_goodness() > r_threshold:
 			self._extend_region(r_extend_by)
 			self._make_fit()
@@ -158,13 +137,11 @@ class FitOptimizer(object):
 				self.show_fit(self.obj)
 				outfunc('The params were:{}\n'.format(self.popt))
 				return self.popt
-				break
 			if self.counter == max_tries:
 				self.show_fit(self.obj)
 				outfunc('Max tries ({}) reached.. try another initial params.\n You can set the bounds at Edit --> Settings.'.format(max_tries))
 				return np.zeros_like(self.popt)
-				break
-				
+
 		while self._fit_goodness() < r_threshold:
 			self._make_fit()
 			self.counter +=1
@@ -172,13 +149,7 @@ class FitOptimizer(object):
 				self.show_fit(self.obj)
 				outfunc('Max tries ({}) reached.. try another initial params.\n You can set the bounds at Edit --> Settings.'.format(max_tries))
 				return np.zeros_like(self.popt)
-				# self.popt = []
-				break
-	
 
-	def report(self):
-		return self.popt, self.counter
-	
 
 # a, b, c, d = np.loadtxt('E:/int_new/Interferometry/examples/teszt.txt', delimiter = ',', unpack = True ) # f.set_initial_region(0.15, 2.5) #f.run_loop(r_extend_by = 0.05, r_threshold = 0.79, max_tries = 20000, show_steps = True)
 # a, b, c, d = np.loadtxt('FOD.txt', delimiter = ',', unpack = True )
