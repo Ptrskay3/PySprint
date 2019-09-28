2eb37e007c7a004adaa666da5ee5c77d4ce43239
Ptrskay3 <leeh123peter@gmail.com>
2019-09-28 11:49:20 +0200

    .

---
+++ tests/test_MainApp.py
@@ -0,0 +1,20 @@
+import sys
+import unittest
+try:
+	from PyQt5.QtTest import QTest
+except:
+	raise ImportError('PyQt5 package is missing.')
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+
+sys.path.append('..')
+# sys.modules['lmfit']=None
+# import lmfit
+from core import evaluate
+
+class TestMainApp(unittest.TestCase):
+
+	def test_SPP(self):
+		a, b = 0, 0 
+		self.assertEqual(a,b)
+		
