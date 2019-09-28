25941c266cb020877f84de93648db4e1f6ebf9d6
Ptrskay3 <leeh123peter@gmail.com>
2019-09-28 11:54:02 +0200

    .

--- tests/test_MainApp.py
+++ tests/test_MainApp.py
@@ -10,11 +10,13 @@ from PyQt5.QtWidgets import QApplication
 sys.path.append('..')
 # sys.modules['lmfit']=None
 # import lmfit
-from core import evaluate
 
 class TestMainApp(unittest.TestCase):
 
 	def test_SPP(self):
 		a, b = 0, 0 
 		self.assertEqual(a,b)
+
+if __name__ == '__main__':
+	unittest.main()
 		
