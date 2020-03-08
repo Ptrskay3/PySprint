6812fec4ed53b7daedfedd22547d6bf069254665
Péter Leéh <leeh123peter@gmail.com>
2020-03-08 10:14:55 +0100

    unused lines

--- pysprint/utils/spp_helpers.py
+++ pysprint/utils/spp_helpers.py
@@ -6,10 +6,10 @@ def correct_sign(x, flip_increasing=True):
 	if not isinstance(x, np.ndarray):
 		x = np.asarray(x)
 	increasing_idx = np.where(np.diff(x) > 0)
-	inc_len = len(increasing_idx[0])
+	#inc_len = len(increasing_idx[0])
 	decreasing_idx = np.where(np.diff(x) < 0)
-	dec_len = len(decreasing_idx[0])
-	stationary_idx = np.where(np.diff(x) == 0)
+	#dec_len = len(decreasing_idx[0])
+	#stationary_idx = np.where(np.diff(x) == 0)
 	increasing = group_consecutives(increasing_idx)
 	decreasing = group_consecutives(decreasing_idx)
 	if not (len(increasing) == 1 and len(decreasing) == 1):
