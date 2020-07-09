7a9f9d3ee6d8c461b9eab0b0a3b5ecfaeb6e782a
Péter Leéh <leeh123peter@gmail.com>
2020-07-09 17:20:27 +0200

    WTF window functions

--- pysprint/core/methods/wft.py
+++ pysprint/core/methods/wft.py
@@ -1,12 +1,14 @@
 from functools import lru_cache, wraps
 
+import numpy as np
+
 from pysprint.core.methods._fft import FFTMethod
 from pysprint.core.evaluate import gaussian_window, cut_gaussian
 
 
 # https://stackoverflow.com/a/54487188/11751294
 
-def mutually_exclusive(keyword, *keywords):
+def mutually_exclusive_args(keyword, *keywords):
     keywords = (keyword,) + keywords
     def wrapper(func):
         @wraps(func)
@@ -25,11 +27,12 @@ class Window:
     Basic class that implements functionality related to Gaussian
     windows.
     """
-    def __init__(self, x, center, fwhm, order=2):
+    def __init__(self, x, center, fwhm, order=2, **kwargs):
         self.x = x
         self.center = center
         self.fwhm = fwhm
         self.order = order
+        self.mpl_style = kwargs
 
     @lazy_property
     def y(self):
@@ -40,7 +43,7 @@ class Window:
         _fwhm = std * 2 * np.log(2) ** (1 / order)
         return cls(x, center, _fwhm, order)
 
-    def plot(self, ax=None, **kwargs):
+    def plot(self, ax=None):
         """
         Plot the window.
 
@@ -49,14 +52,10 @@ class Window:
 
         ax : axis
             The axis to plot on. If not given, plot on the last axis.
-
-        **kwargs : 
-            keyword arguments to pass to matplotlib.pyplot.plot
         """
         if ax is None:
             ax = plt
-        ax.plot(self.x, self.y, **kwargs)
-
+        ax.plot(self.x, self.y, **self.mpl_style)
 
 
 class WFTMethod(FFTMethod):
@@ -67,33 +66,74 @@ class WFTMethod(FFTMethod):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    @mutually_exclusive('std', 'fwhm')
-    def add_window(self, center, std=None, fwhm=None, order=2):
+    @mutually_exclusive_args('std', 'fwhm')
+    def add_window(self, center, std=None, fwhm=None, order=2, **kwargs):
         if std:
-            window = Window.from_std(self.x, center=center, std=std, order=order)
+            window = Window.from_std(self.x, center=center, std=std, order=order, **kwargs)
         else:
-            window = Window(self.x, center=center, fwhm=fwhm, order=order)
+            window = Window(self.x, center=center, fwhm=fwhm, order=order, **kwargs)
         self.window_seq[center] = window
 
     @property
     def windows(self):
         return self.window_seq
 
-    @mutually_exclusive('std', 'fwhm')
-    def add_window_sequence(self, start, stop, step, std, fwhm, order):
+    def add_window_arange(self, start, stop, step, std=None, fwhm=None, order=2, **kwargs):
         """
         Build a window sequence of given parameters to apply on ifg.
+        Works similar to numpy.arange.
         """
-        pass
+        arr = np.arange(start, stop, step)
+        if std:
+            for cent in arr:
+                self.add_window(cent, std=std, order=order, **kwargs)
+        else:
+            for cent in arr:
+                self.add_window(cent, fwhm=fwhm, order=order, **kwargs)
         
-    def view_windows(self):
+
+    @mutually_exclusive_args("std", "fwhm")
+    def add_window_linspace(self, start, stop, num, std=None, fwhm=None, order=2, **kwargs):
+        """
+        Build a window sequence of given parameters to apply on ifg.
+        Works similar to numpy.linspace.
+        """
+        arr = np.linspace(start, stop, num)
+        if std:
+            for cent in arr:
+                self.add_window(center=cent, std=std, order=order, **kwargs)
+        else:
+            for cent in arr:
+                self.add_window(center=cent, fwhm=fwhm, order=order, **kwargs)
+
+    @mutually_exclusive_args("std", "fwhm")
+    def add_window_geomspace(self, start, stop, num, std=None, fwhm=None, order=2, **kwargs):
+        """
+        Build a window sequence of given parameters to apply on ifg.
+        Works similar to numpy.geomspace.
+        """
+        arr = np.geomspace(start, stop, num)
+        if std:
+            for cent in arr:
+                self.add_window(center=cent, std=std, order=order, **kwargs)
+        else:
+            for cent in arr:
+                self.add_window(center=cent, fwhm=fwhm, order=order, **kwargs)
+
+    def view_windows(self, ax=None):
         """
         Gives a rough view of the different windows along with the ifg.
         """
-        pass
+        # TODO: check for too crowded image later and warn user.
+        # Maybe just display a sample that case?
+        for _, val in self.window_seq.items():
+            val.plot(ax=ax)
 
 if __name__ == '__main__':
-    import numpy as np
     import matplotlib.pyplot as plt
     w = WFTMethod(np.arange(100), np.arange(100))
-    w.add_window(center=50, order=2)
+    w.add_window_linspace(20, 70, 100, fwhm=50)
+    w.view_windows()
+    plt.legend()
+    plt.show(block=True)
+    print(w)
