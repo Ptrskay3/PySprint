c79bbaf5f8127c57fc2217ec783f4548094e7782
Péter Leéh <leeh123peter@gmail.com>
2020-07-15 22:31:31 +0200

    corrected pysprint.core.phase.Phase.fit method

--- pysprint/core/phase.py
+++ pysprint/core/phase.py
@@ -29,15 +29,14 @@ class Phase:
     A class that represents a phase obtained from various
     methods.
     """
-
-    is_dispersion_array = False
-    is_coeff = False
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.poly = None
-        self.fit = None
+        self.fitted_curve = None
+        self.is_dispersion_array = False
+        self.is_coeff = False
+        self.fitorder = None
 
     def __call__(self, value):
         if self.poly:
@@ -72,7 +71,7 @@ class Phase:
         if self.poly is not None:
             return self.poly.__str__()
         raise NotImplementedError(
-            "Before calling, a polinomial must be fitted."
+            "Before calling, a polynomial must be fitted."
         )
 
     def plot(self, ax=None, **kwargs):
@@ -80,8 +79,8 @@ class Phase:
             ax = plt
         if not self.is_dispersion_array or not self.is_coeff:
             ax.plot(self.x, self.y, **kwargs)
-            if self.fit is not None:
-                ax.plot(self.x, self.fit, "r--")
+            if self.fitted_curve is not None:
+                ax.plot(self.x, self.fitted_curve, "r--")
         else:
             ax.plot(self.x, self.poly(self.x), **kwargs)
         plt.grid()
@@ -92,42 +91,43 @@ class Phase:
         if self.is_coeff or self.is_dispersion_array:
             warnings.warn("No need to fit another curve.")
             return
-
-        self.fitorder = order
-
-        _function = _fit_config[order]
-
-        x, y = np.copy(self.x), np.copy(self.y)
-        x -= reference_point
-
-        if _has_lmfit:
-
-            fitmodel = Model(_function)
-            pars = fitmodel.make_params(
-                **{f"b{i}": 1 for i in range(order + 1)}
-            )
-            result = fitmodel.fit(y, x=x, params=pars)
         else:
-            popt, pcov = curve_fit(_function, x, y, maxfev=8000)
+            self.fitorder = order
 
-        if _has_lmfit:
-            dispersion, dispersion_std = transform_lmfit_params_to_dispersion(
-                *unpack_lmfit(result.params.items()), drop_first=True, dof=1
-            )
-            fit_report = result.fit_report()
-            self.fit = result.best_fit()
-        else:
-            dispersion, dispersion_std = transform_cf_params_to_dispersion(
-                popt, drop_first=True, dof=1
-            )
-            fit_report = (
-                "To display detailed results,"
-                " you must have `lmfit` installed."
-            )
+            _function = _fit_config[order]
 
-            self.fit = _function(x, *popt)
+            x, y = np.copy(self.x), np.copy(self.y)
+            x -= reference_point
 
-        return dispersion, dispersion_std, fit_report
+            if _has_lmfit:
+
+                fitmodel = Model(_function)
+                pars = fitmodel.make_params(
+                    **{f"b{i}": 1 for i in range(order + 1)}
+                )
+                result = fitmodel.fit(y, x=x, params=pars)
+            else:
+                popt, pcov = curve_fit(_function, x, y, maxfev=8000)
+
+            if _has_lmfit:
+                dispersion, dispersion_std = transform_lmfit_params_to_dispersion(
+                    *unpack_lmfit(result.params.items()), drop_first=True, dof=1
+                )
+                fit_report = result.fit_report()
+                self.fitted_curve = result.best_fit
+            else:
+                dispersion, dispersion_std = transform_cf_params_to_dispersion(
+                    popt, drop_first=True, dof=1
+                )
+                fit_report = (
+                    "To display detailed results,"
+                    " you must have `lmfit` installed."
+                )
+
+                self.fitted_curve = _function(x, *popt)
+
+            return dispersion, dispersion_std, fit_report
+
 
     @property
     def order(self):
