701924119d769e0f97f560030f3dccef84fdc2de
Péter Leéh <leeh123peter@gmail.com>
2020-08-16 10:13:14 +0200

    test conda

--- .ci/conda_upload.sh
+++ .ci/conda_upload.sh
@@ -1,12 +1,14 @@
 PKG_NAME=pysprint
 USER=ptrskay
 
+set -e
+
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
+
 ls -l
 conda build .
-
 
 find $CONDA_BLD_PATH/ -name *.tar.bz2 | while read file
 do
