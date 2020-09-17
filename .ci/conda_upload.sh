b84bb91e3c2a3e087be5aacbf6699e0ff4b04431
Péter Leéh <leeh123peter@gmail.com>
2020-09-17 10:43:08 +0200

    .

--- .ci/conda_upload.sh
+++ .ci/conda_upload.sh
@@ -2,11 +2,17 @@ USER=ptrskay
 
 set -e
 
+array=( 3.6 3.7 3.8 )
+
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 conda config --set ssl_verify no
 export CONDA_BLD_PATH=~/conda-bld
-conda build . --python=3.8
+
+for i in "${array[@]}"
+do
+	conda build . --python $i
+done
 
 find $CONDA_BLD_PATH/ -name *.tar.bz2 | while read file
 do
