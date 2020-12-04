93a47b86ee16a27cbae235ca37ce5d0758063c2c
Péter Leéh <leeh123peter@gmail.com>
2020-12-04 09:50:32 +0100

    test release

--- setup.py
+++ setup.py
@@ -28,42 +28,42 @@ except ImportError:
 from versioneer import get_cmdclass, get_version
 
 cmdclass = get_cmdclass()
-_VersioneerSdist = cmdclass['sdist']
+# _VersioneerSdist = cmdclass['sdist']
 
-class CargoModifiedSdist(_VersioneerSdist):
-    """Modifies Cargo.toml to use an absolute rather than a relative path
+# class CargoModifiedSdist(_VersioneerSdist):
+#     """Modifies Cargo.toml to use an absolute rather than a relative path
 
-    The current implementation of PEP 517 in pip always does builds in an
-    isolated temporary directory. This causes problems with the build, because
-    Cargo.toml necessarily refers to the current version of pyo3 by a relative
-    path.
+#     The current implementation of PEP 517 in pip always does builds in an
+#     isolated temporary directory. This causes problems with the build, because
+#     Cargo.toml necessarily refers to the current version of pyo3 by a relative
+#     path.
 
-    Since these sdists are never meant to be used for anything other than
-    tox / pip installs, at sdist build time, we will modify the Cargo.toml
-    in the sdist archive to include an *absolute* path to pyo3.
-    """
+#     Since these sdists are never meant to be used for anything other than
+#     tox / pip installs, at sdist build time, we will modify the Cargo.toml
+#     in the sdist archive to include an *absolute* path to pyo3.
+#     """
 
-    def make_release_tree(self, base_dir, files):
-        """Stages the files to be included in archives"""
-        super().make_release_tree(base_dir, files)
-        import toml
+#     def make_release_tree(self, base_dir, files):
+#         """Stages the files to be included in archives"""
+#         super().make_release_tree(base_dir, files)
+#         import toml
 
-        # Cargo.toml is now staged and ready to be modified
-        cargo_loc = os.path.join(base_dir, "Cargo.toml")
-        assert os.path.exists(cargo_loc)
+#         # Cargo.toml is now staged and ready to be modified
+#         cargo_loc = os.path.join(base_dir, "Cargo.toml")
+#         assert os.path.exists(cargo_loc)
 
-        with open(cargo_loc, "r") as f:
-            cargo_toml = toml.load(f)
+#         with open(cargo_loc, "r") as f:
+#             cargo_toml = toml.load(f)
 
-        rel_pyo3_path = cargo_toml["dependencies"]["pyo3"]["path"]
-        base_path = os.path.dirname(__file__)
-        abs_pyo3_path = os.path.abspath(os.path.join(base_path, rel_pyo3_path))
-        cargo_toml["dependencies"]["pyo3"]["path"] = abs_pyo3_path
+#         rel_pyo3_path = cargo_toml["dependencies"]["pyo3"]["path"]
+#         base_path = os.path.dirname(__file__)
+#         abs_pyo3_path = os.path.abspath(os.path.join(base_path, rel_pyo3_path))
+#         cargo_toml["dependencies"]["pyo3"]["path"] = abs_pyo3_path
 
-        with open(cargo_loc, "w") as f:
-            toml.dump(cargo_toml, f)
+#         with open(cargo_loc, "w") as f:
+#             toml.dump(cargo_toml, f)
 
-cmdclass["sdist"] = CargoModifiedSdist
+# cmdclass["sdist"] = CargoModifiedSdist
 
 setup(
     name="pysprint",
