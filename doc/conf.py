18b7c4c6342fbf8fe0d24b8c663010ef452e182a
Péter Leéh <leeh123peter@gmail.com>
2021-01-29 13:51:57 +0100

    0.21 build

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ project = 'Pysprint'
 copyright = '2020, Peter Leeh'
 author = 'Peter Leeh'
 
-release = '0.20.0'
+release = '0.21.0'
 
 autodoc_default_options = {
     'special-members': '__init__'
