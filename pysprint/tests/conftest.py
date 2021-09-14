5dae815a3db0a5b839b4e1cc1fa4a1e786bd80e8
Leeh Peter <leeh123peter@gmail.com>
2021-09-14 12:45:32 +0200

    update azure ci

--- pysprint/tests/conftest.py
+++ pysprint/tests/conftest.py
@@ -1,10 +1,6 @@
-# This should be enabled later
 import os
 import platform
 import pytest
 
 no_osx = pytest.mark.skipif(platform.system() == 'Darwin', reason="There is no xvfb on osx.")
-no_mpl = pytest.mark.skipif(os.environ["RUNNER_OS"] == "Linux", reason="There is no xvfb setup on Linux")
-# @pytest.fixture(scope="session", autouse=True)
-# def execute_before_any_test():
-#     matplotlib.use("Qt5Agg")
+no_mpl = pytest.mark.skipif(os.environ.get("RUNNER_OS", None) == "Linux", reason="There is no xvfb setup on Linux")
